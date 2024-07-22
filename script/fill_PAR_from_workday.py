96a2a743b951c4ccf46e65c646b2ada6263e9290
Jean Quinn <xxj8xg@virginia.edu>
2024-07-22 19:35:34 -0400

    fixed bug in organize_data and started fill_document

--- script/fill_PAR_from_workday.py
+++ script/fill_PAR_from_workday.py
@@ -4,6 +4,23 @@ from pdf2docx import parse
 import os 
 import re
 
+
+
+def parse_workday_docx(document: Document):
+    table = document.tables[1]
+    data = []
+    keys = None
+    for i, row in enumerate(table.rows):
+        text = [cell.text for cell in row.cells]
+        if i == 0:
+            continue
+        if i == 1:
+            keys = tuple(text)
+        row_data = dict(zip(keys, text))
+        data.append(row_data)
+    return data
+
+
 def fetch_workday_data():
     # create docx in directory
     docx_path = "./unfilled-reports/test.docx"
@@ -20,15 +37,23 @@ def fetch_workday_data():
 
     # save and parse dat
     document = Document(docx_path)
-    name, start_date, end_date = document.paragraphs[1].text.split('\n')
-    start_date = start_date[-10:]
-    end_date = end_date[-10:]
     raw_data = parse_workday_docx(document)
-    organize_data(raw_data)
+
+    # get employee info
+    name, start_date, end_date = document.paragraphs[1].text.split('\n')
+    start_date = start_date[-10:].strip()
+    end_date = end_date[-10:].strip()
+    name = name.strip()
+
+    # organize dat
+    return raw_data, name, start_date, end_date
+
 
 def organize_data(raw_data):
-    # organizes data into : [comment, date, hours logged]
-    # combines entries that are logged on the same day
+    '''
+    returns data in format : [comment, date, hours logged]
+    combines entries that are logged on the same day
+    '''
     entries = []
     temp = []
     r = re.compile('[0-9]*/[0-9]*/[0-9]*')
@@ -39,7 +64,7 @@ def organize_data(raw_data):
         else:
             if r.match(entry['Date']) is not None:
                 # date for the current entry
-                if len(temp) > 2:
+                if len(temp) >= 2:
                     continue
                 temp.append(entry['Date'])
             elif h.match(entry['Date']) is not None:
@@ -50,69 +75,49 @@ def organize_data(raw_data):
                 temp = []
             else:
                 # comment for the current entry
-                if len(temp) == 2:
+                if len(temp) >= 2:
                     temp[0] += ', ' + entry['Comment']
-                temp.append(entry['Comment'])
+                else:
+                    temp.append(entry['Comment'])
     return entries
 
+# NOTE FORGERY CONCERNS WITH SIGNATURE, DISCUSS LATER
+def fill_document(data, name, start_date, end_date):
+    PAR_path = "./par-template/PAR-template.docx"
+    PAR = Document(PAR_path)
 
+    # name
+    PAR.tables[2].rows[0].cells[1].text.replace("XX", name)
+    # period beginning
+    PAR.tables[2].rows[2].cells[3].text = start_date
+    # period ending
+    PAR.tables[2].rows[2].cells[5].text = end_date
 
-
-def parse_workday_docx(document: Document):
-    table = document.tables[1]
-    data = []
-    keys = None
-    for i, row in enumerate(table.rows):
-        text = [cell.text for cell in row.cells]
-        if i == 0:
-            continue
-        if i == 1:
-            keys = tuple(text)
-        row_data = dict(zip(keys, text))
-        data.append(row_data)
-    return data
-    # for row in data:
-    #     print("\n")
-    #     print(row)
-
-
-    # FROM ORIG: date, comment, quantity
-    # just look at reported time
-
-    # TO NEW DOC: date, detailed activity description, hours worked
-    # 0 6 8
-
-
-
-
-# fill the Word doc
-# def fill_document(data):
-#     doc_path = "/mnt/data/Personnel Activty Report Template (1).doc"
-#     doc = Document(doc_path)
+    if len(data) > 20:
+        exit("Your Workday pdf output has too many time entries (over 20) for the report. Please adjust and resubmit. Exiting")
     
-#     # find/fill in the fields (this will depend on the exact structure of your data)
-#     for para in doc.paragraphs:
-#         if "Name/Title:" in para.text:
-#             para.text = para.text.replace("XX", data['employee_name'])
-#         if "Reporting Period:" in para.text:
-#             para.text = para.text.replace("(Beginning)", data['start_date'])
-#             para.text = para.text.replace("(Ending)", data['end_date'])
+    # sublist indices: 0 = description, 1 = date, 2 = hours worked
+    for i, sublist in enumerate(data):
+        for j, item in enumerate(sublist):
+            print(j, item)
+            PAR.tables[3].rows[i+1].cells[j].text = item
     
-#     table = doc.tables[0]  # Assuming there is only one table
-#     for i, entry in enumerate(data['entries']):
-#         row = table.add_row()
-#         row.cells[0].text = entry['date']
-#         row.cells[1].text = entry['description']
-#         row.cells[2].text = str(entry['hours'])
+    name = name.replace(" ", "")
+    filename = f'./filled-reports/PAR-{name}-{start_date}.docx'
+    # TODO make file with filename
 
-#     # save the filled document
-#     doc.save("/Filled_Personnel_Activity_Report.docx")
+    # then save
+    PAR.save(filename)
 
-# main function to execute the script
+
 def main():
-    data = fetch_workday_data()
-    # if data:
-    #     fill_document(data)
+    raw_data, name, start_date, end_date = fetch_workday_data()
+    if raw_data:
+        organized_data = organize_data(raw_data)
+        fill_document(organized_data, name, start_date, end_date)
+    else:
+         exit(1, "Fetching workday data failed. Exiting")
+
 
 if __name__ == "__main__":
     main()
